9a61834923bece5f459c4fb317b98fa1a3b4a122
chenhe-henry <chenhe.elec@gmail.com>
2021-10-26 22:44:23 +1100

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function isHenry(string) {
-  return string === "Henry";
+function postcodeValidation(string) {
+  if (typeof string === "number" && string.toString().length === 4) {
+    return { res: true, msg: "This is a correct postcode." };
+  } else {
+    return { res: false, msg: "Please enter a 4-digit postcode." };
+  }
 }
 
-module.exports = isHenry;
+module.exports = postcodeValidation;
